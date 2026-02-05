a7535bcb65f8ffdb5ba389a672b42fc14472b055
Sayed Saminur Rahman <37567927+sayedsamin@users.noreply.github.com>
2026-02-05 10:46:59 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello! This is Version 1 of the Express App.</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
